d146701c4826c54d11a55449307403c41ad02f36
Robin Vinod <robinvvinod@gmail.com>
2020-12-03 14:02:43 +0800

    removed README.md

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,7 +10,7 @@ export default function Footer() {
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Made with ❤️ by SMA. All credits and attribution available at ...")}
+          {emoji("Made with ❤️ by SMA. All credits and attribution available at https://github.com/robinvvinod/smakathon")}
         </p>
       </div>
     </Fade>
